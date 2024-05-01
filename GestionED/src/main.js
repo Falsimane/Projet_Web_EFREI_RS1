7f8582c4b9e54c5e085fa885094116292ba31a8d
Falsimane <falsimagne.thomas@gmail.com>
2024-05-01 13:47:53 +0200

    Page de login

--- GestionED/src/main.js
+++ GestionED/src/main.js
@@ -1,7 +1,7 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import router from './router'
 
 const app = createApp(App)
